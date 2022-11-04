db88297adb2aef206c93b6629180134626bcfa96
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-04 14:37:54 +0200

    save progress

--- src/components/content/card/index.js
+++ src/components/content/card/index.js
@@ -5,17 +5,22 @@ function Card (props) {
  const classNames = [
   'card'
  ];
- console.log(props.isVisible);
  // if (props.isVisible == false) {
  if (!props.isVisible) {
   classNames.push('hide');
  }
  return (
   <div className={classNames.join(' ')}>
-   <div className="card-image" style={{
-    backgroundImage: `url(${props.image})`
-   }}></div>
-   <div className="card-title">{props.title}</div>
+   <div
+    onClick={props.setSelectedCard}
+    className="card-image"
+    style={{
+     backgroundImage: `url(${props.image})`
+    }}></div>
+   <div
+    onClick={props.setSelectedCard}
+    className="card-title"
+   >{props.title}</div>
    <div className="card-description">{props.description}</div>
    <div className="card-date-section">
     {props.date} |
--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,49 +1,11 @@
 import React, { useState } from "react";
 import './style.css';
-import image1 from './imgs/5e4b1a8c95e491e2e6265a89_photo-1441512673622-3eaa1c39ba28.jpg';
-import image2 from './imgs/5e4b1a8c95e491b7e5265a8a_3f4eb74a.jpg';
-import image3 from './imgs/5e4b1a8c95e4916f85265a88_photo-1433854304641-67729357fe18.jpg';
-import image4 from './imgs/5e4b1a8c95e4911851265a87_photo-1443466612971-98a16c828136.jpg';
 import Card from './card';
+import Article from './article';
+import mockCards from './mock/cards.json';
 
 function Content () {
- const cards = [
-  {
-   image: image1,
-   title: 'Coffee variety macchiato, as organic ut variety caffeine americano',
-   description: 'Saucer, crema carajillo, bar, mocha medium, latte cappuccino and espresso acerbic to go. Coffee, irish foam turkish coffee blue mountain seasonal. Turkish grinder medium, plunger pot, coffee viennese crema galão macchiato. Filter, cinnamon, caffeine in, cortado, plunger pot decaffeinated cinnamon lungo con panna milk',
-   date: 'June 25, 2020',
-   tag: 'Travel',
-  },
-  {
-   image: image2,
-   title: 'Overlaid the jeepers uselessly much excluding',
-   description: 'Saucer dark cup french press. Barista beans, organic eu variety skinny acerbic flavour caffeine white. Steamed, plunger pot ut dripper decaffeinated, caramelization, sit barista mug con panna shop turkish.',
-   date: 'June 25, 2020',
-   tag: 'Tech',
-  },
-  {
-   image: image3,
-   title: 'Wherever far wow thus a squirrel raccoon jeez jaguar this from along',
-   description: 'Cup, caramelization viennese, cultivar macchiato flavour percolator ristretto frappuccino. Lungo extra macchiato cream chicory turkish caramelization.',
-   date: 'June 25, 2020',
-   tag: 'Art',
-  },
-  {
-   image: image4,
-   title: 'According a funnily until pre-set or arrogant well cheerful',
-   description: 'Single shot cultivar beans as chicory caffeine. Medium brewed, milk extra that froth pumpkin spice mocha. Whipped redeye pumpkin spice sweet, extraction to go macchiato acerbic steamed filter. Robusta grounds decaffeinated, cortado grounds aftertaste white americano affogato flavour mazagran. Aged eu, extraction, cup seasonal frappuccino in seasonal saucer wings.',
-   date: 'June 25, 2020',
-   tag: 'Music',
-  },
-  {
-   image: image4,
-   title: 'According a funnily until pre-set or arrogant well cheerful',
-   description: 'Single shot cultivar beans as chicory caffeine. Medium brewed, milk extra that froth pumpkin spice mocha. Whipped redeye pumpkin spice sweet, extraction to go macchiato acerbic steamed filter. Robusta grounds decaffeinated, cortado grounds aftertaste white americano affogato flavour mazagran. Aged eu, extraction, cup seasonal frappuccino in seasonal saucer wings.',
-   date: 'June 25, 2020',
-   tag: 'Music',
-  }
- ];
+ const cards = mockCards;
 
  const [selectedTag, setSelectedTag] = useState();
  console.log('selectedTag:', selectedTag);
@@ -76,6 +38,8 @@ function Content () {
  //  }
  //  return true;
  // });
+ const [selectedCard, setSelectedCard] = useState();
+ console.log('selectedCard:', selectedCard);
  return (
   <div className="content-container">
    <div
@@ -83,7 +47,7 @@ function Content () {
     style={selectedTagStyle}
    >{selectedTag}</div>
    {
-    cards.map(el => {
+    !selectedCard && cards.map(el => {
      let isVisible = true;
      if (selectedTag) {
       isVisible = selectedTag === el.tag;
@@ -96,15 +60,35 @@ function Content () {
       tag={el.tag}
       date={el.date}
       setTag={setSelectedTag}
+      setSelectedCard={() => setSelectedCard(el)}
      />);
     })
    }
+   {selectedCard && <Article card={selectedCard}/>}
    <div
     className="return-button"
-    onClick={() => setSelectedTag()}
+    onClick={() => {
+     setSelectedTag();
+     setSelectedCard();
+    }}
    >All posts</div>
   </div>
  );
 }
 
 export default Content;
+
+/**
+ * let a = 45;
+ * let b = 67;
+ * let c;
+ * c = a && b; // => 67      ✓
+ *                   45 + 67
+ *                   4567
+ *                   45
+ *                   error 
+ * c = a || b; // => 45      ✓
+ * 
+ * c = ("" || "abc") && { v: 1 }; // => "abc"
+ *                                      { v: 1 }  ✓
+ */
