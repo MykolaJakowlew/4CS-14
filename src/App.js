cd64547e3e2e6620a75ee4dc0d73de83bf7797c5
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-18 14:39:41 +0200

    save progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import WidgetWeather from './components/widget-weather';
 
 function App () {
+  const [cityName, setCityName] = useState();
+  const onBlur = (event) => {
+    const value = event.target.value;
+    console.log(value);
+    setCityName(value);
+  };
+
   return (
     <div className="App">
+      <input
+        onBlur={onBlur}
+        type="text"
+        placeholder='Input city name'
+      />
+      <WidgetWeather city={cityName} />
     </div>
   );
 }
